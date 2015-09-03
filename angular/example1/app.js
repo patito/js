8f36b80c7ab775696973abbecd8fab94252b8778
patito <benatto@gmail.com>
2015-09-03 09:25:29 +0100

    fixing some bugs

--- angular/example1/app.js
+++ angular/example1/app.js
@@ -1,7 +1,10 @@
-(function() {
+(function () {
+    'use strict';
+
     var gem = { name: 'Azurite', price: 2.95 };
     var app = angular.module('gemStore', []);
-    app.controller('StoreController', function() {
-        this.product = gem;
+    app.controller('StoreController', function ($scope) {
+        $scope.world = "caceta";
+        $scope.ppk = "pererek";
     });
 }());
